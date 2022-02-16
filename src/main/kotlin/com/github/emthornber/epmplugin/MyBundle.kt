c3e7fa4687238062785ec50075df9a0c30dcac23
GitHub Action <action@github.com>
2022-02-16 13:27:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/emthornber/epmplugin/MyBundle.kt
+++ src/main/kotlin/com/github/emthornber/epmplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.emthornber.epmplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/emthornber/epmplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/emthornber/epmplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.emthornber.epmplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.emthornber.epmplugin.MyBundle
 
 class MyApplicationService {
 
